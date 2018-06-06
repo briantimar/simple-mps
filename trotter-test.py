0448ac0b9df9ad36baeadb1b3ae2f27bb29b074f
brian <timarbrian@gmail.com>
2018-06-06 15:43:22 -0700

    layer generator

--- trotter-test.py
+++ trotter-test.py
@@ -11,7 +11,11 @@ from mps import MPS
 from mps import MPO_from_local_matrix
 from mps import MPS_from_product_states
 
+from trotter import ExpPauliHamiltonian
 
+import sys
+sys.path.append("/home/brian/Documents/ryd-theory-code/ryd-theory/python_code/")
+from ryd_base import make_1d_TFI_static
 
 ###check product state construction
 
@@ -33,22 +37,32 @@ def test_product_state():
 ### check imaginary-time trotter
     
 from trotter import get_pauli_exp, sigmax
-U = get_pauli_exp(1.0j, 'X')
 
-L=10
-D=5
-sps=2
-psi = MPS(L, sps=sps)
-psi.init_random(D)
-i=2
-psi.normalize(i)
-from trotter import _act_1qubit_gate
-nstep = 100
-sigma_x = MPO_from_local_matrix(sigmax(), i)
-for _ in range(nstep):
-    _act_1qubit_gate(U, psi, i)
-    psi._normalize_site(i)
-    print(sigma_x._expt_value_local(psi))
+def test_imag_onesite():
+    U = get_pauli_exp(-1.0j, 'X')
     
+    L=10
+    D=5
+    sps=2
+    psi = MPS(L, sps=sps)
+    psi.init_random(D)
+    i=2
+    psi.normalize(i)
+    from trotter import _act_1qubit_gate
+    nstep = 100
+    sigma_x = MPO_from_local_matrix(sigmax(), i)
+    for _ in range(nstep):
+        _act_1qubit_gate(U, psi, i)
+        psi._normalize_site(i)
+        print(sigma_x._expt_value_local(psi))
+        
+    
+ 
+L=4
+static = make_1d_TFI_static(4, 1, L)
+expH = ExpPauliHamiltonian(static)
+expH.set_layer(['X'])
+expH.set_layer(['ZZ'])
+
     
     
--- trotter.py
+++ trotter.py
@@ -44,12 +44,12 @@ def get_pauli_exp(t, opstr):
          
          Note the i.
          """
-    return np.cos(t) * np.identity(2) + 1j * np.sin(t) * get_pauli_mat(opstr)
+    return np.cos(t) * np.identity(2) - 1j * np.sin(t) * get_pauli_mat(opstr)
 
 def get_tensor_prod(ops):
     op = ops[0]
     for i in range(1, len(ops)):
-        op = np.tensordot(op, ops[i])
+        op = np.tensordot(op, ops[i],axes=0)
     return op
 
 def get_pauli_prod(opstr_list):
@@ -59,6 +59,7 @@ def get_pauli_prod(opstr_list):
     return get_tensor_prod(ops)
 
 def get_exp_pauli_prod(t, opstr_list):
+
     """ Returns exp(i t Sigma1...SigmaN), where Sigmai are the Pauli ops specified in opstr_list"""
     exp_ops = [get_pauli_exp(t, o) for o in opstr_list]
     return get_tensor_prod(exp_ops)
@@ -98,25 +99,142 @@ def _act_2qubit_local_gate(U, psi, i):
     psi.set_sites([i, i+1], [A1_tilde, A2_tilde])
         
     
+    
+def _is_commuting(op1, op2):
+    if op1.upper() in ['X'] and op2.upper() in ['Z', 'N']:
+        return False
+    return True
 
 class LocalHamiltonian(object):
     """ A hamiltonian for a 1d system which is never fully stored in memory.
         Assumed to be a quasi-local; the local terms can be obtained in matrix representation on-demand."""
         
-    def __init__(self, L, sps):
+    def __init__(self, sps, static_list):
         """ L = chain length
             sps = states per site.
+            static_list = a quspin-format static opstr list which fully specifies the hamiltonian
             """
-        self.L = L
+
         self.sps = sps
-        self._terms_by_k = dict()
+        self._static_list = static_list
+        self._layers = []
+        self._by_opstr = dict()
+        for s in self._static_list:
+            s[0] = s[0].upper()
+            opstr, couplings = s[0], s[1]
+            if opstr not in self._by_opstr.keys():
+                self._by_opstr[opstr] = couplings
+            else:
+                self._by_opstr[opstr] += couplings
+    
+    def set_layer(self, opstrs):
+        """ define a particular layer which is to be exponentiated all at once"""
+        self._layers.append([o.upper() for o in opstrs])
+            
+    def get_all_terms(self, opstr_list):
+        """ returns static list involving only the ops specified."""
+        terms = []
+        for o in opstr_list:
+            terms.append( [o.upper(), self._by_opstr[o.upper()]] )
+        return terms
+    
+class ExpPauliHamiltonian(LocalHamiltonian):
+    """A trotterized exponential of hamiltonian in the pauli basis."""
+    
+    def __init__(self, static_list):
+        LocalHamiltonian.__init__(self, 2, static_list)
+    
+    def _get_term_iterator(self, opstr, t):
+        return ExpTermIterator( self.get_all_terms([opstr]), get_pauli_exp_gen(t))
+    
+    def _get_layer_iterator(self, layer, t):
+        for opstr in layer:
+            for (U,sites) in self._get_term_iterator(opstr, t):
+                yield (U,sites)
+    
+    def layers(self, t):
+        """Iterator over all gate layers."""
+        for layer in self._layers:
+            yield self._get_layer_iterator(layer, t)
+
+    
+    
         
-    def get_klocal_terms(self, k):
-        return self._terms_by_k[k]
+def ExpTermIterator(static_list, exp_gen):
+    """Generator which produces actual exponentials.
+        exp_gen: a function which, given an opstr and a coupling strength, returns an exponential of the corresponding hamiltonian term.
+        
+        yields: tuples (U, sites) of unitaries and the corresponding sites they act on.
+             """
+    
+    for opstr, couplings in static_list:
+        for c in couplings:
+            J, sites = c[0], c[1:]
+            yield (exp_gen(opstr, J), sites)
+            
+            
+def get_pauli_exp_gen(t):
+    """ Returns a function which produces exponentials of Pauli matrices corresponding to a fixed Trotter time"""
+    def pauli_exp_gen(opstr, J):
+        """ opstr = quspin-style opstr, like 'Z' or 'nn'.
+        J = a coupling strength
+        
+        Returns: exponential of the corresponding hamiltonian as np array
+        ASSUMES THE TWO OPERATORS COMMUTE -- exponentiates each and then takes the tensor product"""
+        opstr_list = [a for a in opstr]
+        
+        return get_exp_pauli_prod(t*J, opstr_list)
+    return pauli_exp_gen
+
+
+    
+class TrotterLayers(object):
+    """ Stores trotter layers for a particular hamiltonian"""
+    
+    def __init__(self, local_hamiltonian):
+        self.H = local_hamiltonian
+        self.evolve_time = None
+        self.num_layers = None
+        
+    def set_evolve_time(self, T):
+        self.evolve_time= T
+    def set_num_layers(self, n):
+        self.num_layers = n
+    def _get_dt(self):
+        return self.evolve_time / self.num_layers
+    
+    
+    
+    
+def run_trotter_evol(trot_layers, psi, T, nlayer=100, renormalize=False):
+    """ Run trotter evolution on state psi (updates in-place).
+        trot_layers: provides two (for now) noncommuting trotter layers.
+        psi = MPS pure state.
+        T = total evolution time.
+        If renormalize=True: renormalize the state after layer application. """
+        
+    trot_layers.set_evolve_time(T)
+    trot_layers.set_num_layers(nlayer)
+    for _ in range(nlayer):
+        for layer in trot_layers.layers:
+            layer.apply(psi)
+            if renormalize:
+                psi.left_normalize_full()
+                
+                
+                
+    
         
     
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
